cd128f52d24b96c1e2b0565ef37643014f111090
Jorge Escobar <j@jorge3.com>
2016-02-24 21:24:14 +0000

    adding fields and indexes to user model

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Step 5
 
-## Implementing the user registration
+## Adding new fields and indexes to models
+- Modify the user model and talk about how we can add fields to user models in mongodb
+ - Add a bio
+ - Add a couple of users
+
+- Indexes
+ - Do `db.user.find({'u': 'jorge'}).explain()` and explain how indexes are used (`"cursor" : "BasicCursor", "nscannedObjects" : 3,`)
+ - Set username and email as unique, explain unique
+ - Add indexes (explain why indexes)
+ - Do `python manage.py shell` and update the user and then check `db.user.getIndexes()`
+ - Do `db.user.find({'u': 'jorge'}).explain()` and explain how indexes are used (`"cursor" : "BtreeCursor u_1", "nscannedObjects" : 1,`)
 
 
 
--- user/models.py
+++ user/models.py
@@ -3,9 +3,16 @@ from application import db
 from utilities.common import utc_now_ts as now
 
 class User(db.Document):
-    username = db.StringField(db_field="u", required=True)
+    username = db.StringField(db_field="u", required=True, unique=True)
     password = db.StringField(db_field="p", required=True)
-    email = db.EmailField(db_field="e", required=True)
+    email = db.EmailField(db_field="e", required=True, unique=True)
     first_name = db.StringField(db_field="fn", max_length=50)
     last_name = db.StringField(db_field="ln", max_length=50)
+    bio = db.StringField(db_field="bio", max_length=50)
     created = db.IntField(db_field="c", default=now())
+    
+    meta = {
+        'indexes': ['username', 'email', '-created']
+        }
+    
+    
