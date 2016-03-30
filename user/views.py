cb96dd99193e750f2d9cd15a0f4be0f63f05bb3d
Jorge Escobar <j@jorge3.com>
2016-03-30 22:02:48 +0000

    Finished password change

--- user/views.py
+++ user/views.py
@@ -158,7 +158,6 @@ def forgot():
     
 @user_app.route('/password_reset/<username>/<code>', methods=('GET','POST'))
 def password_reset(username, code):
-    message = None
     require_current = False
     
     form = PasswordResetForm()
@@ -182,12 +181,42 @@ def password_reset(username, code):
         
     return render_template('user/password_reset.html', 
         form=form,
-        message=message, 
         require_current=require_current,
         username=username,
         code=code
         )
-        
+
 @user_app.route('/password_reset_complete')
 def password_reset_complete():
     return render_template('user/password_change_confirmed.html')
+
+@user_app.route('/change_password', methods=('GET','POST'))
+def change_password():
+    require_current = True
+    error = None
+    form = PasswordResetForm()
+
+    user = User.objects.filter(username=session.get('username')).first()
+    if not user:
+        abort(404)
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            if bcrypt.hashpw(form.current_password.data, user.password) == user.password:
+                salt = bcrypt.gensalt()
+                hashed_password = bcrypt.hashpw(form.password.data, salt)
+                user.password = hashed_password
+                user.save()
+                # if user is logged in, log him out
+                if session.get('username'):
+                    session.pop('username')
+                return redirect(url_for('user_app.password_reset_complete'))
+            else:
+                error = "Incorrect password"
+        
+    return render_template('user/password_reset.html', 
+        form=form,
+        require_current=require_current,
+        error=error
+        )
+
