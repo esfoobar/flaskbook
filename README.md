80fcdcacafc883fb1947b121865ec8edbe27bfa3
Jorge Escobar <j@jorge3.com>
2016-03-30 19:41:12 +0000

    Finished forgot password functionality, now to write test

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ## Forgot password reset functionality
     - Create a form for it
     - Create a view to enter your email and email is sent
-    -> Create a view to enter the new password
-    - Write a test for it
+    - Create a view to enter the new password
+    -> Write a test for it
     
 ## Reset password view
     - Create a form for it
--- user/views.py
+++ user/views.py
@@ -156,10 +156,36 @@ def forgot():
         message = "You will receive a password reset email if we find that email in our system"
     return render_template('user/forgot.html', form=form, error=error, message=message)
     
-@user_app.route('/password_reset/<username>/<code>')
-@user_app.route('/password_reset', methods=('POST'))
+@user_app.route('/password_reset/<username>/<code>', methods=('GET','POST'))
 def password_reset(username, code):
-    error = None
     message = None
+    require_current = False
+    
     form = PasswordResetForm()
-    return render_template('user/password_reset.html', form=form, error=error, message=message)
+
+    user = User.objects.filter(username=username).first()
+    if not user or code != user.change_configuration.get('password_reset_code'):
+        abort(404)
+
+    if request.method == 'POST':
+        del form.current_password
+        if form.validate_on_submit():
+            salt = bcrypt.gensalt()
+            hashed_password = bcrypt.hashpw(form.password.data, salt)
+            user.password = hashed_password
+            user.change_configuration = {}
+            user.save()
+            session.pop('username')
+            return redirect(url_for('user_app.password_reset_complete'))
+        
+    return render_template('user/password_reset.html', 
+        form=form,
+        message=message, 
+        require_current=require_current,
+        username=username,
+        code=code
+        )
+        
+@user_app.route('/password_reset_complete')
+def password_reset_complete():
+    return render_template('user/password_change_confirmed.html')
