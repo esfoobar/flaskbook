9d4c6f84c307bcdd91214f041c75eb8f4c2eb3ff
Jorge Escobar <j@jorge3.com>
2016-05-13 19:24:38 +0000

    modifying git move script

--- git-move.sh
+++ git-move.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
 
-BRANCHNAME=$1
-TAGNAME=$2
+# BRANCHNAME=$1
+# TAGNAME=$2
 
-echo "Request to convert the branch ${BRANCHNAME} to a tag with the same name accepted."
-echo "Processing..."
-echo " "
+# echo "Request to convert the branch ${BRANCHNAME} to a tag with the same name accepted."
+# echo "Processing..."
+# echo " "
 
-git show-ref --verify --quiet refs/heads/${BRANCHNAME}
-# $? == 0 means local branch with <branch-name> exists. 
+# git show-ref --verify --quiet refs/heads/${BRANCHNAME}
+# # $? == 0 means local branch with <branch-name> exists. 
 
-if [ $? == 0 ]; then
-   git checkout ${BRANCHNAME}
-   git tag ${BRANCHNAME}
-   git checkout master
-   git branch ${BRANCHNAME} -d
-   echo " "
-   echo "Updated list branches, sorted chronologically: "
-   echo "---------------------------------------------- "
-   git log --no-walk --date-order --oneline --decorate $(git rev-list --branches --no-walk) | cut -d "(" -f 2 | cut -d ")" -f 1
-else
-   echo "Sorry. The branch ${BRANCHNAME} does NOT seem to exist. Exiting."
-fi
+# if [ $? == 0 ]; then
+#   git checkout ${BRANCHNAME}
+#   git tag ${BRANCHNAME}
+#   git checkout master
+#   git branch ${BRANCHNAME} -d
+#   echo " "
+#   echo "Updated list branches, sorted chronologically: "
+#   echo "---------------------------------------------- "
+#   git log --no-walk --date-order --oneline --decorate $(git rev-list --branches --no-walk) | cut -d "(" -f 2 | cut -d ")" -f 1
+# else
+#   echo "Sorry. The branch ${BRANCHNAME} does NOT seem to exist. Exiting."
+# fi
+
+max=1
+for i in `seq 1 $max`
+do
+    echo "Tagging step $i"
+    git tag step-${i} refs/heads/step-${i}
+done
